08d25062b79344d0ecf3ef1e9a13f94df61c2364
Jade Asmus <jadeasmus99@gmail.com>
2022-08-19 21:46:17 -0700

    init new version

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`h-full w-full flex justify-center items-center`}>
+      <Text style={tw`text-red-500`}>
+        Open up App.js to start working on your app!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
